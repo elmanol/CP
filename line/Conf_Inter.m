73994d5ecfa6a8e5cb3c82db66874a43c868560a
Manolis Kerimakis <kerimakis@ceid.upatras.gr>
2019-03-05 00:57:22 +0200

    pc

--- line/Conf_Inter.m
+++ line/Conf_Inter.m
@@ -14,7 +14,7 @@ end
 for i=1:90
     xx(:,i) = every_iter_power3(:,i);% Create Data
     SEM = std(xx(:,i))/sqrt(length(xx(:,i)));    % Standard Error
-    ts = tinv([0.025  0.975],length(xx(i))-1);    % T-Score
+    ts = tinv([0.025  0.975],length(xx(:,i))-1);    % T-Score
     CI_Discrete3(:,i) = mean(xx(:,i)) + ts*SEM;    % Confidence Intervals
 end
 for i=1:90
@@ -32,12 +32,10 @@ boxplot(CI_Discrete1, 'colors','g','MedianStyle','target')
 set(gca,'XTickLabel',{' '})
 boxplot(CI_Discrete2, 'colors','r','MedianStyle','target')
 set(gca,'XTickLabel',{' '})
-% boxplot(CI_2, 'colors','b','MedianStyle','target')
-% set(gca,'XTickLabel',{' '})
 boxplot(CI_Discrete3,'colors','m','MedianStyle','target')
 set(gca,'XTickLabel',{' '})
 boxplot(CI_Discrete4, 'colors','k','MedianStyle','target')
-
+set(gca,'XTickLabel',{' '})
 
 %hold off;
 xlabel('Time (rounds)')
